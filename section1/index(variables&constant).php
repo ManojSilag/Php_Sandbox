5ddde2ce75cb7c03d14c2f134848f2cb7477f43c
Manoj <silagmanoj@yahoo.in>
2019-01-31 21:00:01 +0530

    Adding files

---
+++ section1/index(variables&constant).php
@@ -0,0 +1,24 @@
+<?php 
+
+  //echo 'hello, Manoj';
+
+  // Variables
+  //$name = 'Manoj';
+  $age = 22;
+  // $name = "Silag";
+  
+  //define constants
+  define('NAME', 'Manoj')
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>My first PHP file</title>
+ </head>
+ <body>
+   <h1><?php echo 'Hello, Manoj'; ?></h1>
+   <div><?php echo NAME;  ?></div>
+   <div><?php echo $age ; ?></div>
+ </body>
