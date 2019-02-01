acd237d6cd95065a105c239895ade7a697e291a1
Manoj <silagmanoj@yahoo.in>
2019-02-01 22:14:49 +0530

    Adding files

---
+++ section1/index(strings).php
@@ -0,0 +1,46 @@
+<?php 
+
+  $stringOne = 'my email is ';
+  $stringTwo = 'manojsilag@gmail.com';
+  
+  //echo $stringOne;
+
+  //concantiating two string use ==> . (dot)
+	  //echo $stringOne . $stringTwo;
+
+	  $name = 'Manoj';
+	  //echo 'Hey, my name is ' . $name;
+	  
+	  //using double courts "" we can declare inside a variable
+	  //echo "Hey my name is $name";
+
+	  //escaping
+      //echo "Cat meowed, \"Mwauu\""; 
+      //echo 'Cat meowed, "Mwauu"';  //alternative to above
+    
+    //getting character out of string
+    //echo $name[0];
+   
+   //functions
+    //echo strlen($name);
+
+	//echo strtoupper($name);
+
+	//echo strtolower($name);
+
+	echo str_replace('j', 'W', $name);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Php Strings</title>
+</head>
+<body>
+    
+
+</body>
+</html>
