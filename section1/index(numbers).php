e8ad5554c25cf82b0e9b443ec9fd1095d3295d78
Manoj <silagmanoj@yahoo.in>
2019-02-05 13:39:24 +0530

    Adding files

---
+++ section1/index(numbers).php
@@ -0,0 +1,41 @@
+<?php
+
+$radius = 25;
+$pi = 3.14;
+
+    //Basic Operators: *, /, +, -, **
+
+    //echo $pi * $radius **2;
+
+    //order of Operation ( B I D M A S ) 
+    //echo 2 * (4 + 9) / 3;
+
+    //Increment & Decrement operators
+    $radius++;
+    //echo $radius;
+
+    //shorthand operators
+    $age = 20;
+    $age +=10;
+    $age *= 2;
+    //echo $age;
+
+    //number functions
+    //echo floor($pi)
+    //echo ceil($pi);
+
+    echo pi();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="sass.css">
+	<title>Php Numbers</title>
+</head>
+<body>
+    
+
+</body>
+</html>
