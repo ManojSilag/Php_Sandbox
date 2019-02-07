a53ff6b1b405fa41e8837f420b1eb3951406816c
Manoj <silagmanoj@yahoo.in>
2019-02-07 16:09:38 +0530

    adding files

---
+++ section2/index(booleans&comparisons).php
@@ -0,0 +1,45 @@
+<?php 
+
+//comparisons booleans(true or false)
+ 
+//echo true; "1"
+//echo false; ""
+
+//numbers
+	// echo 5 < 10;
+	// echo 5 > 10;
+	// echo 5 == 10;
+	// echo 10 == 10;
+	// echo 5 != 10;
+	// echo 5 <= 5;
+	// echo 5 >=5;
+
+//stringd
+	//echo 'shaun' < 'yoshi';
+	//echo 'shaun' > 'youshi';
+	//echo 'manoj' > 'Manoj';
+	//echo 'mario' == 'mario';
+	//echo 'mario' == 'mario';
+
+//loose vs strict equal comparisons
+
+//echo 5 == '5';
+//echo 5 === '5';
+
+//echo true == '1';
+echo false == '';
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="sass.css">
+	<title>Php booleans&comparisons</title>
+</head>
+<body>
+    
+
+</body>
+</html>
