b2856deb2ba52819d3fd76f807b872b62b769901
Manoj <silagmanoj@yahoo.in>
2019-02-13 16:12:11 +0530

    add files

---
+++ section3/1(include&requre)/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+include('second.php');
+//require('second.php');
+
+
+
+echo "end of php";
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Php include and require</title>
+ </head>
+ <body>
+    <?php include 'content.php' ?>
+    <?php include 'content.php' ?>
+ </body>
+ </html>
