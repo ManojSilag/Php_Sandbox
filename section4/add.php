1223cebb679c51cccafbb4a0950c916040ff974b
Manoj Silag <silagmanoj@yahoo.in>
2019-02-18 20:12:44 +0530

    Adding basic form validation

--- section4/add.php
+++ section4/add.php
@@ -8,11 +8,36 @@
   // }
 
   if(isset($_POST['submit'])){
-  	echo htmlspecialchars($_POST['email']);
-  	echo htmlspecialchars($_POST['title']);
-  	echo htmlspecialchars($_POST['intgredients']);
+  	// echo htmlspecialchars($_POST['email']);
+  	// echo htmlspecialchars($_POST['title']);
+  	// echo htmlspecialchars($_POST['intgredients']);
 
-  }
+  	//check email
+  	if(empty($_POST['email'])){
+  		echo "An email is required <br>";
+  	}else{
+  		echo htmlspecialchars($_POST['email']);
+  	}
+
+
+  	//check title
+  	if(empty($_POST['title'])){
+  		echo "An title is required <br>";
+  	}else{
+  		echo htmlspecialchars($_POST['title']);
+  	}
+
+
+  	//check integredients
+  	if(empty($_POST['integredients'])){
+  		echo "An integredients is required <br>";
+  	}else{
+  		echo htmlspecialchars($_POST['integredients']);
+  	}
+
+
+  } //end of post check
+
 
  ?>
 
@@ -25,7 +50,7 @@
 	<h4 class="center">Add a Pizza</h4>
 	<form class="white" action="add.php" method="POST">
 		<label>Your Email:</label>
-		<input type="text" name="email">
+		<input type="email" name="email">
 		<label>Pizza title</label>
 		<input type="text" name="title">
 		<label>Intgredients (comma separated):</label>
