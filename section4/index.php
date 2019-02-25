45298ea90383a1c94f54d7e4a27ca10ec240119f
Manoj Silag <silagmanoj@yahoo.in>
2019-02-25 21:18:43 +0530

    #getting data from database

--- section4/index.php
+++ section4/index.php
@@ -12,6 +12,29 @@
   	echo "connection error" . mysql_connect_error();
   }
 
+//Getting data from a Database
+
+  //Write query for all pizzas
+  // $sql = 'SELECT * FROM pizzas'; // *  is used to select all column
+
+  $sql = 'SELECT title , intgredients,id FROM pizzas';
+
+  //make query and get result
+  $result = mysqli_query($conn, $sql);
+
+  //fetch the resulting rows as an array
+  $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  //free the memory for result variable
+  mysqli_free_result($result);
+
+  //close connection
+  mysqli_close($conn);
+  
+  print_r($pizzas);
+
+
+
  ?>
 
  <!DOCTYPE html>
