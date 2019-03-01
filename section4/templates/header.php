77e6f575e5903a0ee08ba73754c6d9a04f9b5e12
Manoj Silag <silagmanoj@yahoo.in>
2019-03-01 21:37:43 +0530

    #making header link work

--- section4/templates/header.php
+++ section4/templates/header.php
@@ -22,8 +22,8 @@
 
   	<nav class="white z-depth-0">
   		<div class="container">
-  			<a href="#" class="center brand-logo brand-text">Manoj Pizza</a>
+  			<a href="index.php" class="center brand-logo brand-text">Manoj Pizza</a>
   			<ul id="nav-mobile" class="right  hide-on-small-and-down">
-  				<li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+  				<li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
   			</ul>
   	</nav>
