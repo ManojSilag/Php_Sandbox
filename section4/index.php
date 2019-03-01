674164f121e33d0e5533659d5733e84f2a3789fb
Manoj Silag <silagmanoj@yahoo.in>
2019-03-01 21:34:55 +0530

    #update

--- section4/index.php
+++ section4/index.php
@@ -49,17 +49,17 @@
 
  <div class="container">
  	<div class="row">
- 		<?php foreach($pizzas as $pizza){ ?>
+ 		<?php foreach($pizzas as $pizza): ?>
 
  			<div class="col s6 md3">
  				<div class="card z-depth-0">
  					<div class="card-content center">
  						<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
  						<ul>
- 							<?php foreach (explode(',', $pizza['intgredients']) as $ing){ ?>
+ 							<?php foreach (explode(',', $pizza['intgredients']) as $ing): ?>
  								<li><?php echo htmlspecialchars($ing); ?></li>
 
- 							<?php } ?>	
+ 							<?php endforeach ?>	
  						</ul>
  					</div>
  					<div class="card-action right-align">
@@ -69,7 +69,7 @@
  			</div>
 
 
- 		<?php } ?>
+ 		<?php endforeach; ?>
  	</div>
  </div>
 
