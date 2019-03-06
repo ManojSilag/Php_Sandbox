3180d4701296f81d8d02242a2c0ce99a2fa97833
Manoj Silag <silagmanoj@yahoo.in>
2019-03-06 20:06:09 +0530

    #ternary operator

---
+++ section3/index(ternary operator).php
@@ -0,0 +1,31 @@
+<?php 
+
+
+//ternary operators
+
+ $score = 50;
+// if($score > 40){
+// 	echo "high score";
+// }else{
+// 	echo "less score";
+// }
+
+//  $val = $score > 40 ? 'high score!' : 'Low score';
+// echo $val;
+
+ // echo $score > 40 ? 'high score!' : 'Low score';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php tuts</title>
+</head>
+<body>
+	<p>
+	<?php  echo $score > 40 ? 'high score!' : 'Low score'; ?>
+	</p>
+
+</body>
+</html>
